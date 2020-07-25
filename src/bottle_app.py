a019c9da9c1282703d113c1627a959fbfccb64cc
resquinelato <54194209+resquinelato@users.noreply.github.com>
2020-07-24 23:58:13 -0300

    Adiciona uma nova rota no bottle_app

--- src/bottle_app.py
+++ src/bottle_app.py
@@ -2,19 +2,26 @@
 # bottle_app.py
 # SPDX-License-Identifier: GPL-3.0-or-later
 """ Tutorial Dois - Brincando de git.
+
 .. codeauthor:: Rodrigo Esquinelato <resquinelato@gmail.com>
+
+Sem classes neste modulo:
 
 Changelog
 ---------
 .. versionadded::    20.07
-        Documentação do tutorial.
+        Adiciona o gerencidor de comandos http via Bottle
 """
-# A very simple Bottle Hello World app for you to get started with...
+
 from bottle import default_app, route
 
 @route('/')
 def hello_world():
     return 'Tutorial Dois - Aprendendo Git e Botlle'
+
+@route('/oi')
+def oi_mundo():
+    return 'Tutorial Dois - Ensaiando uma nova rota'
 
 application = default_app()
 
