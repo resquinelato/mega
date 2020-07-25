457fbe12552715639aa7f914e33322fd52cea957
resquinelato <resquinelato@gmail.com>
2020-07-25 14:27:35 +0000

    Added suffix_name = '.rst'

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -56,4 +56,9 @@ html_theme = 'default'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# The master toctree document
 master_doc = 'index'
+
+# The suffix of source filename
+suffix_name = '.rst'
