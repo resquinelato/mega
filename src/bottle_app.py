eabb7cee8f5827827beb18d9c683e6c4797a7141
resquinelato <resquinelato@gmail.com>
2020-07-27 07:41:15 +0000

    Atualiza documentação de botle_app.py

--- src/bottle_app.py
+++ src/bottle_app.py
@@ -19,27 +19,44 @@ from main import Main
 
 @route('/')
 def hello_world():
+    """Roteia o ensaio o jogo do quarto.
+    """
     return static_file('index.html', root='/home/resquinelato/dev/mega/src/', mimetype='text/html')
 
 @route('/oi')
 def oi_mundo():
+    """Roteia o ensaio de uma nova rota.
+    """
     return 'Tutorial Dois - Ensaiando uma nova rota'
 
 @route('/vs')
 def vs_mundo():
-    """Roteia o caminho /vs para retornar a versão do sistema."""
+    """Roteia o caminho /vs para retornar a versão do sistema.
+    """
     return 'Tutorial Dois - Versão do sistema: {}'.format(Main().get_versao())
 
 @route('/<filename:re:.*\.py>')
 def py_mundo(filename):
+    '''Cria um rota com o caminho /qualquer_coisa/arquivo.py e retorna os arquivos.py.
+
+       :param filename: Nome do arquivo.
+    '''
     return static_file(filename, root='/home/resquinelato/dev/mega/src/', mimetype='text/python')
 
 @route('/doc/<filename:re:.*\.html>')
 def doc_mundo(filename):
+    '''Cria um rota com o caminho /qualquer_coisa/arquivo.html e retorna os arquivos.html.
+
+       :param filename: Nome do arquivo.
+    '''
     return static_file(filename, root='/home/resquinelato/dev/mega/docs/build/html', mimetype='text/html')
 
 @route('/doc/<filename:re:.*\.css>')
 def css_mundo(filename):
+    '''Cria um rota com o caminho /qualquer_coisa/arquivo.css e retorna os arquivos.css.
+
+       :param filename: Nome do arquivo.
+    '''
     return static_file(filename, root='/home/resquinelato/dev/mega/docs/build/html/', mimetype='text/css')
 
 
